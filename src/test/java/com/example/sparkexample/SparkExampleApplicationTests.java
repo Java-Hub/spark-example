182cb611a6ec44432d67de7e50e4475de71b494e
caik <caik@esensoft.com>
2020-10-16 17:08:12 +0800

    1

---
+++ src/test/java/com/example/sparkexample/SparkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sparkexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
